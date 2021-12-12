3375d89ad58c407fe4c050089f11a94bf8fa5429
jayeshjeh <jay.parmar.11169@gmail.com>
2021-12-12 20:14:07 +0530

    commit

--- Functions/App.py
+++ Functions/App.py
@@ -1,14 +1,11 @@
-def Addition(value1, value2):
-    result =0
-    result = value1 + value2
-    return result
+import Marvellous
 
 def main():
     print("Inside module :",__name__)
     no1 = int(input("Enter first number : "))
     no2 = int(input("Enter second number : "))
 
-    ret = Addition(no1, no2)
+    ret = Marvellous.Addition(no1, no2)
 
     print("Addition is : ",ret)
 
