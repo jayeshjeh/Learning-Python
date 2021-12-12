6a3c3e2d96a653705c8cd70943d6aab17a63d311
jayeshjeh <jay.parmar.11169@gmail.com>
2021-12-12 20:30:43 +0530

    commit

--- Functions/App.py
+++ Functions/App.py
@@ -1,4 +1,5 @@
 import Marvellous
+import Infosystems
 
 def main():
     print("Inside module :",__name__)
@@ -6,8 +7,10 @@ def main():
     no2 = int(input("Enter second number : "))
 
     ret = Marvellous.Addition(no1, no2)
-
     print("Addition is : ",ret)
+
+    ret = Infosystems.Subtraction(no1,no2)
+    print("Subtraction is :",ret)
 
 if __name__ == "__main__":
     main()
