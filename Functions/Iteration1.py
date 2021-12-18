e97a25d2b1dfd6ba67a5f2e761f86a0eeb06a044
jayeshjeh <jay.parmar.11169@gmail.com>
2021-12-18 21:22:24 +0530

    commit

---
+++ Functions/Iteration1.py
@@ -0,0 +1,20 @@
+def Iteration():
+    no =0
+
+    while(no < 5):
+        print("Hello")
+        no = (no + 1)
+
+def Sequence():
+    print("Hello")
+    print("Hello")
+    print("Hello")
+    print("Hello")
+    print("Hello")
+
+def main():
+    #Sequence()
+    Iteration()
+
+if __name__ == "__main__":   # == comparison operator
+    main()
--- Functions/Selection.py
+++ Functions/Selection.py
@@ -1,3 +1,4 @@
+
 def Maximum(value1, value2):
     if(value1 > value2):
         return value1
