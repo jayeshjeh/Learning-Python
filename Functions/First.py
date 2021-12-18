670d0c0c70861cfe56672e6dcf383f69e3f5039b
jayeshjeh <jay.parmar.11169@gmail.com>
2021-12-18 19:49:12 +0530

    commit

---
+++ Functions/First.py
@@ -0,0 +1,11 @@
+def main():
+    print("Inside main")
+
+print("One")
+
+if __name__ =="__main__":
+    print("Inside starter")
+    main()
+    print("End of Python Application")
+
+print("Hello")
---
+++ Functions/new.py
@@ -0,0 +1,3 @@
+for i in range(4):
+    if i: print(i, end = ' ')
+
