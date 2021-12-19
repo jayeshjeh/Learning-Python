8320e174af1375c2afb45179f4845daa37ec5b23
jayeshjeh <jay.parmar.11169@gmail.com>
2021-12-19 12:58:39 +0530

    commit

---
+++ Assignment/1.3.py
@@ -0,0 +1,18 @@
+
+def Addition(value1, value2):
+    result = value1 + value2
+
+    return result
+
+def main():
+    print("Enter First number")
+    no1 = int(input())
+
+    print("Enter Second Number")
+    no2 = int(input())
+
+    ret = Addition(no1,no2)
+    print("Addition is : ",ret)
+
+if __name__ == "__main__":
+    main()
