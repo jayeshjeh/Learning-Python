0c8eb2c2d79715e0ab34e46bf9638f1f66369d00
jayeshjeh <jay.parmar.11169@gmail.com>
2021-12-19 13:11:02 +0530

    commit

---
+++ Assignment/1.4.py
@@ -0,0 +1,9 @@
+def Run():
+    for i in range(5):
+        print("Marvellous")
+
+def main():
+    Run()
+
+if __name__ == "__main__":
+    main()
---
+++ Assignment/1.5.py
@@ -0,0 +1,9 @@
+def Iteration():
+    for i in range(10,0,-1):
+        print(i)
+
+def main():
+    Iteration()
+
+if __name__ == "__main__":
+    main()
