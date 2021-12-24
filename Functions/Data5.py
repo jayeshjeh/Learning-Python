97d5c11b9d5a1c940b9d617d73a5cbfbfc74abfa
jayeshjeh <jay.parmar.11169@gmail.com>
2021-12-24 19:38:47 +0530

    commit

---
+++ Functions/Data5.py
@@ -0,0 +1,9 @@
+Data = [10, 20, 30, 40]
+
+print(Data[-1])
+
+print(Data)
+
+Data.insert(4, 50)
+
+print(Data)
---
+++ Functions/tuple.py
@@ -0,0 +1,24 @@
+# Tuple
+
+# Hetrogeneous
+# Indexed
+# Ordered
+# Allows Duplicate
+# Data of tuple is immutable
+Data = (10, 20, 30, 40, "Marvellous", 10)
+
+print("Data type is : ", type(Data))
+
+print("Elements are : ", Data)
+
+print(" 0th element is : ", Data[0])
+
+print("Element at 3rd index : ", Data[3])
+
+print("Last element is : ", Data[-1])
+
+#Data[0] = 11
+
+#Data.append(50)
+
+#print("Updated data is : ", Data)
