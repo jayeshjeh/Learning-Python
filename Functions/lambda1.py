0cfb7e6cf719e222cd0149fe7e0b723751c505df
jayeshjeh <jay.parmar.11169@gmail.com>
2021-12-26 21:13:08 +0530

    commit

---
+++ Functions/lambda1.py
@@ -0,0 +1,21 @@
+# named function
+def Add(a, b):
+    return a + b
+
+# lambda function
+AddX = lambda a, b : a+b
+
+def main():
+    ret = Add(10, 20)
+    print("Addition is : ", ret)
+
+    ret = AddX(10, 30)
+    print("Addition is : ", ret)
+    print(type(Add))
+    print(type(AddX))
+    print(type(lambda a, b : a+b))
+
+if __name__ == "__main__":
+    main()
+
+
