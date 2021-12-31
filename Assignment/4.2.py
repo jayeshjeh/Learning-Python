c2e3a3f4b48785be42762273dd0b5554898f8cef
jayeshjeh <jay.parmar.11169@gmail.com>
2021-12-31 23:49:04 +0530

    commit

--- Assignment/4.2.py
+++ Assignment/4.2.py
@@ -1,13 +1,14 @@
-
-Power = lambda no1, no2: no1 * no2
+def gun(no1, no2):
+    return lambda no1, no2: no1 * no2
 
 def main():
     no1 = int(input("Enter first number"))
     no2 = int(input("Enter second number"))
 
+
     x = lambda no1, no2 : no1 * no2
 
-    newdata = (Power, no1,no2)
+    newdata = gun(no1, no2)
     print(newdata)
 
 
