3c7b2569465ac36bcd4f3bb8ba875cbc39d56060
jayeshjeh <jay.parmar.11169@gmail.com>
2021-12-31 16:36:42 +0530

    commit

--- Assignment/noll.py
+++ Assignment/noll.py
@@ -2,3 +2,23 @@
 age = 24
 print("My age is " + str(age) + " years")
 
+
+print(f" PI value is approx {22/7}")
+
+first = 'Jayesh'
+last = 'Parmar'
+age = 26
+
+print(first + last + str(age))
+
+print(45-15/3)
+
+days = "Mon, Tue, Wed, Thu, Fri, Sat, Sun"
+
+print(days[::5])
+
+data = "1:A, 2:B, 3:C, 4:D, 5:E, 6:F, 7:G, 8:H"
+print(data[0:-1:5])
+
+flower = "blue voilet"
+print('blue' in flower)
