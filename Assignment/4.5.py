49656b0830fe22ed3d08e6d6b0c05d5a903e4cd3
jayeshjeh <jay.parmar.11169@gmail.com>
2022-01-01 16:19:28 +0530

    commit

---
+++ Assignment/4.5.py
@@ -0,0 +1,29 @@
+from functools import reduce
+
+primenumber =
+
+
+def main():
+    print("Enter length of numbers")
+    no1 = int(input())
+
+    data = []
+
+    for i in range(no1):
+        print("Enter numbers ")
+        no = int(input())
+        data.append(no)
+
+    print("Input data is: ", data)
+
+    newdata = list(filter(primenumber, data))
+    print("Data after filter: ", newdata)
+
+    squrno = list(map(lambda a: (a*a), newdata))
+    print("Data after filter: ", squrno)
+
+    ret = reduce(lambda a, b: a + b, squrno)
+    print("Data after reduce: ", ret)
+
+if __name__ == "__main__":
+    main()
