f3dd1f24bd3a2a2f6ae88c525b4e529fb0dcec9e
jayeshjeh <jay.parmar.11169@gmail.com>
2022-01-01 22:13:21 +0530

    commit

---
+++ Functions/add1.py
@@ -0,0 +1,6 @@
+no1 = 11.5
+no2 = 12
+
+ans = no1 + no2
+
+print("Addition is ",ans)
---
+++ Functions/datatype.py
@@ -0,0 +1,14 @@
+no = 11
+
+name = "Jayesh"
+
+marks = 3.14
+
+print(no)
+print(name)
+print(marks)
+
+print(type(no))
+print(type(name))
+print(type(marks))
+
