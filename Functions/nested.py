3cba6c403751b5a4da916dcbbeae143dfee9959e
jayeshjeh <jay.parmar.11169@gmail.com>
2022-01-08 19:24:42 +0530

    commit

---
+++ Functions/nested.py
@@ -0,0 +1,14 @@
+
+def outer():
+    print("Inside Outer function")
+
+    def Inner():
+        print("Inside Inner function")
+
+    Inner()
+
+def main():
+    outer()
+
+if __name__ == "__main__":
+    main()
