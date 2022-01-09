4f6d1b8541f1b48aa14a8afd58c62dac55fc959d
jayeshjeh <jay.parmar.11169@gmail.com>
2022-01-09 21:39:28 +0530

    commit

---
+++ Functions/exception_handling.py
@@ -0,0 +1,16 @@
+def division(a, b):
+    ans = 0.0
+    ans = a / b
+    return ans
+
+
+def main():
+    no1 = int(input("Enter first number: "))
+    no2 = int(input("Enter second number: "))
+
+    ret = division(no1, no2)
+    print("Division is: ", ret)
+
+
+if __name__ == "__main__":
+    main()
