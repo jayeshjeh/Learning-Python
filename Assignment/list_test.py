216791c91b2f3a773e0d1343b883e306b1e03ad6
jayeshjeh <jay.parmar.11169@gmail.com>
2022-01-11 23:24:31 +0530

    commit

---
+++ Assignment/list_test.py
@@ -0,0 +1,17 @@
+J = [123, 'xyz', 3.14]
+print(len(J))
+
+print(J[-1])
+print(J + [5, 6, 7])
+print(J * 2)
+J.append("Jayesh")
+print(J)
+print(J)
+
+J.pop(2)
+print(J)
+
+print(J.pop(0))
+print(J)
+J.sort()
+print(J)
