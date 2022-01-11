b6edf5f6604e09703454babb58e9fcee3dd52d18
jayeshjeh <jay.parmar.11169@gmail.com>
2022-01-11 20:49:19 +0530

    commit

--- Assignment/test.py
+++
@@ -1,4 +0,0 @@
-import requests
-resp = requests.get("http://olympus.realpython.org")
-html = resp.text
-print(html[86:132])
---
+++ Assignment/test_int.py
@@ -0,0 +1,17 @@
+import math
+import random
+print(random)
+print(random.random())
+we = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9])
+print(we)
+
+print(math.sqrt(85))
+
+print(5 * 6)
+print(222+5)
+print(2 ** 100)
+print(len(str(2 ** 100)))
+print(3.1415 * 2)
+print(repr(3.1415 * 2))
+a = 3.1415 * 2
+print(a)
