916e34fee327e25e810aa48c3c53166c59d1eca2
jayeshjeh <jay.parmar.11169@gmail.com>
2022-01-13 23:11:01 +0530

    commit

---
+++ Assignment/test_sets.py
@@ -0,0 +1,14 @@
+x = set('spam')
+y = {'h', 'a', 'm'}
+
+print(x, y)
+
+print(x & y)
+print(x | y)
+print(x - y)
+print(x > y)
+
+kik = list(set([1, 2, 1, 3, 2, 5, 1]))
+print(kik)
+print(set('spam') - set('ham'))
+print(set('spam') == set('aspm'))
