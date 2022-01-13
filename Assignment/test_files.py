e57f5eba42c16bd18ff3d733ea7362d8ade40b7b
jayeshjeh <jay.parmar.11169@gmail.com>
2022-01-13 22:27:00 +0530

    commit

---
+++ Assignment/test_files.py
@@ -0,0 +1,16 @@
+f = open('lola.py', 'w')
+f.write('Hello\n')
+
+f.write("world\n")
+f.close()
+
+f = open('lola.py')
+text = f.read()
+print(text)
+print(text.split())
+
+print(dir(f))
+print(help(f.seek))
+
+print('***')
+for line in open('lola.py') : print(line)
