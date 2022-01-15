b86e6d97c00150695def121e4ae86e08cca1a012
jayeshjeh <jay.parmar.11169@gmail.com>
2022-01-15 21:02:33 +0530

    commit

---
+++ Functions/multiprocessing1.py
@@ -0,0 +1,32 @@
+import os
+import multiprocessing
+
+
+def fun(x):
+    print("PID of fun: ", os.getpid())
+    print("PPID of fun: ", os.getppid())
+    print("Inside fun")
+    for i in range(x):
+        print("Fun: ", i)
+
+
+def gun(x):
+    print("PID of gun: ", os.getpid())
+    print("PPID of gun: ", os.getppid())
+    print("Inside gun")
+    for i in range(x):
+        print("Gun: ", i)
+
+
+def main():
+    no = 5
+    print("PID of parent process: ", os.getpid())
+    process1 = multiprocessing.Process(target=fun, args=(no,))
+    process2 = multiprocessing.Process(target=gun, args=(no,))
+
+    process1.start()
+    process2.start()
+
+
+if __name__ == "__main__":
+    main()
