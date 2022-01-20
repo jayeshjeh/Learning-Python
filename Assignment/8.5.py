16157ca353ec913b1ba7233310e8d69d703beb3b
jayeshjeh <jay.parmar.11169@gmail.com>
2022-01-20 14:27:57 +0530

    commit

---
+++ Assignment/8.5.py
@@ -0,0 +1,28 @@
+import threading
+
+
+def display():
+    print("Order 1 to 50: ")
+    for i in range(1, 51):
+        print(i)
+
+
+def rev():
+    print("Reverse 1 to 50: ")
+    for i in range(50, 0, -1):
+        print(i)
+
+
+def main():
+    thread1 = threading.Thread(target=display)
+    thread2 = threading.Thread(target=rev)
+
+    thread1.start()
+    thread1.join()
+
+    thread2.start()
+    thread2.join()
+
+
+if __name__ == "__main__":
+    main()
