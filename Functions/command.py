6e957acac938473d73b9ac09bca6656c456dc10b
jayeshjeh <jay.parmar.11169@gmail.com>
2022-01-22 20:05:42 +0530

    commit

---
+++ Functions/command.py
@@ -0,0 +1,14 @@
+from sys import *
+
+
+def main():
+    print("Number of command line arguments are: ", len(argv))
+    print("Name of Application is: ", argv[0])
+    print(type(argv[1]))
+
+    ans = int(argv[1]) + int(argv[2])
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
