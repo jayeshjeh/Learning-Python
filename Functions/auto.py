27ad053c9eb5361422eaab06963065ddb8edc885
jayeshjeh <jay.parmar.11169@gmail.com>
2022-01-22 20:51:26 +0530

    commit

---
+++ Functions/auto.py
@@ -0,0 +1,34 @@
+
+from sys import *
+
+
+def addition(no1, no2):
+    ans = no1 + no2
+    return ans
+
+
+def main():
+    print('-----------------Marvellous: Automation 1------------------')
+    print('Script name: ', argv[0])
+    print('Number of arguments accepted: ', len(argv) - 1)
+
+    if (len(argv) > 3) or (len(argv) < 2):
+        print("Invalid number of arguments")
+        exit()
+
+    if argv[1] == '-u' or argv[1] == '-U':
+        print("Usage: Script is used to perform the addition of 2 numbers")
+        exit()
+
+    if (argv[1] == '-h') or (argv[1] == '-H'):
+        print("Help: Name_of_Script First_Argument Second_argument")
+        print("First_Argument: Any numeric value")
+        print("Second_Argument: Any numeric value")
+        exit()
+
+    ret = addition(int(argv[1]), int(argv[2]))
+    print("Addition is: ", ret)
+
+
+if __name__ == "__main__":
+    main()
