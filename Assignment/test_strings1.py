a975f4283ef3c80e125c436839828e86e7bfaaf2
jayeshjeh <jay.parmar.11169@gmail.com>
2022-01-26 18:44:09 +0530

    commit

--- Assignment/test_strings1.py
+++ Assignment/test_strings1.py
@@ -1,3 +1,8 @@
 print("Tell me your password: ")
 no1 = input()
 print("The first letter you entered was: ", no1[0].upper())
+
+new = input()
+print(new)
+print(new.find('x'))
+print(new.replace('a', 'j'))
