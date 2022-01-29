13cd77627fa2009e443089257b5f055e414bcedd
jayeshjeh <jay.parmar.11169@gmail.com>
2022-01-29 20:50:55 +0530

    commit

---
+++ Functions/process1.py
@@ -0,0 +1,30 @@
+import psutil
+
+
+def ProcessDisplay():
+    list_process = []
+
+    for proc in psutil.process_iter():
+        try:
+            pinfo = proc.as_dict(attrs=['pid', 'name', 'username'])
+            pinfo['vms'] = proc.memory_info().vms / (1024 * 1024)
+
+            list_process.append(pinfo)
+
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+
+    return list_process
+
+
+def main():
+    print("process monitor")
+
+    list_process = ProcessDisplay()
+
+    for elem in list_process:
+        print(elem)
+
+
+if __name__ == "__main__":
+    main()
