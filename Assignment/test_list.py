3ae734019aaede0b39404ce58aa0f023395188ca
jayeshjeh <jay.parmar.11169@gmail.com>
2022-02-01 15:20:05 +0530

    commit

---
+++ Assignment/test_list.py
@@ -0,0 +1,25 @@
+even = [2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+
+even.extend(odd)
+print(even)
+
+another_even = even
+print(another_even)
+
+even.sort(reverse=True)
+print(even)
+
+print(another_even)
+
+
+page = "The quick brown fox jumps over the lazy dog"
+
+letters = sorted(page)
+print(letters)
+
+numbers = [2.3, 4.5, 8.7, 3.1, 9.2, 1.6]
+sorted_num = sorted(numbers)
+print(sorted_num)
+numbers.sort()
+print(numbers)
