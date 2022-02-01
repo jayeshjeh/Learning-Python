884e660debfe19baa499877ba5d67b95a47035d2
jayeshjeh <jay.parmar.11169@gmail.com>
2022-02-01 15:32:20 +0530

    commit

--- Assignment/test_list.py
+++ Assignment/test_list.py
@@ -21,5 +21,18 @@ print(letters)
 numbers = [2.3, 4.5, 8.7, 3.1, 9.2, 1.6]
 sorted_num = sorted(numbers)
 print(sorted_num)
+
 numbers.sort()
 print(numbers)
+
+missing_letters = sorted("The quick brown fox jumps over the lazy dog", key=str.casefold)
+print(missing_letters)
+names = ["Graham",
+         "John",
+         "terry",
+         "eric",
+         "Terry",
+         "michael"]
+
+names.sort(key=str.casefold)
+print(names)
