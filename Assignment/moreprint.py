a4269a394e1f7ba859b4b55b749a1b8906dc1bef
jayeshjeh <jay.parmar.11169@gmail.com>
2022-02-04 14:08:36 +0530

    commit

---
+++ Assignment/moreprint.py
@@ -0,0 +1,5 @@
+name = 'Jay'
+age = 25
+
+print(name, age, 'Python', 2022)
+print(name, age, 'Python', 2022, sep=", ")
---
+++ Assignment/spam.py
@@ -0,0 +1,44 @@
+menu = [
+    ["egg", "bacon"],
+    ["egg", "sausage", "bacon"],
+    ["egg", "spam"],
+    ["egg", "bacon", "spam"],
+    ["egg", "bacon", "sausage", "spam"],
+    ["spam", "bacon", "sausage", "spam"],
+    ["spam", "sausage", "spam", "bacon", "spam", "tomato", "spam"],
+    ["spam", "egg", "spam", "spam", "bacon", "spam"],
+]
+
+for meal in menu:
+    if 'spam' not in meal:
+        print(meal)
+
+        for item in meal:
+            print(item)
+    else:
+        print("{0} has a spam score of {1}".format(meal, meal.count("spam")))
+
+
+menu = [
+    ["egg", "bacon"],
+    ["egg", "sausage", "bacon"],
+    ["egg", "spam"],
+    ["egg", "bacon", "spam"],
+    ["egg", "bacon", "sausage", "spam"],
+    ["spam", "bacon", "sausage", "spam"],
+    ["spam", "sausage", "spam", "bacon", "spam", "tomato", "spam"],
+    ["spam", "egg", "spam", "spam", "bacon", "spam"],
+]
+
+for meal in menu:
+    for i in range(len(meal) - 1, - 1, - 1):
+        if meal[i] == "spam":
+            del meal[i]
+
+    print(meal)
+
+for meal in menu:
+    for item in meal:
+        if item != "spam":
+            print(item, end=", ")
+    print()
