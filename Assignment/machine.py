b7b5773d1d08556cc4a3b8c9db9bdcf3dea7b038
jayeshjeh <jay.parmar.11169@gmail.com>
2022-02-05 02:01:03 +0530

    commit

---
+++ Assignment/machine.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import sklearn
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+print(f"TensorFlow has access to the following devices:\n{tf.config.list_physical_devices()}")
+
+print(f"TensorFlow version: {tf.__version__}")
