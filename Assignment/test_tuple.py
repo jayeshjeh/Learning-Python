87f46327e8ac46b4af677fce8b0288b399f7fd77
jayeshjeh <jay.parmar.11169@gmail.com>
2022-02-09 19:41:32 +0530

    commit

--- Assignment/test_tuple.py
+++ Assignment/test_tuple.py
@@ -1,12 +1,23 @@
 new = 'Ride the Lightning', 'New toy', 1998
 
 print(new)
-print(new[0])
-print(new[1])
-print(new[2])
+# print(new[0])
+# print(new[1])
+# print(new[2])
+
+title, artist, year = new
+print(title)
+print(artist)
+print(year)
 
 new1 = list(new)
 print(new1)
 
 new1[1] = "Hello Jayesh"
 print(new1)
+
+table = ("coffee table", 200, 100, 75, 34.50)
+print(table[1] * table[2])
+
+name, length, width, height, price = table
+print(length * width)
