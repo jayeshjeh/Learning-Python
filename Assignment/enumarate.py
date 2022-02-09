65471d0dce0dadf49f095c65a7f35b6bc5770f75
jayeshjeh <jay.parmar.11169@gmail.com>
2022-02-09 19:11:51 +0530

    commit

---
+++ Assignment/enumarate.py
@@ -0,0 +1,11 @@
+
+# for i, character in enumerate("abcdefghij"):
+#     print(i, character)
+
+for t in enumerate("abcdefgh"):
+    index, character = t
+    print(index, character)
+
+index, character = (0, "a")
+print(index)
+print(character)
