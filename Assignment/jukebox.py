f64f894285a51827570b3da1b42e889c3207f2ce
jayeshjeh <jay.parmar.11169@gmail.com>
2022-02-22 19:51:39 +0530

    commit

--- Assignment/jukebox.py
+++ Assignment/jukebox.py
@@ -1,13 +1,22 @@
 from test_tuplenested import albums
 
+songs_list = 3
+
 while True:
     print("Please choose your album (invalid choice exits):")
-    # for index, (title, artist, year, songs) in enumerate(albums):
-    #     print("{}: {}, {}, {}, {}".format(index + 1, title, artist, year, songs))
+    for index, (title, artist, year, songs) in enumerate(albums):
+        print("{}: {}".format(index + 1, title))
 
-    for index, value in enumerate(albums):
-        title, artist, year, songs = value
-        print(index, title, artist, year, songs)
+    # for index, value in enumerate(albums):
+    #     title, artist, year, songs = value
+    #     print(index, title, artist, year, songs)
 
-    break
+    choice = int(input())
+    if 1 <= choice <= len(albums):
+        songs_list = albums[choice - 1][3]
+    else:
+        break
 
+    print(albums[choice - 1])
+    print(songs_list)
+    print()
