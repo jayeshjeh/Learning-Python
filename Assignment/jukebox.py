9208efa9a01a8a54df21776ca7037c497c92a400
jayeshjeh <jay.parmar.11169@gmail.com>
2022-02-22 19:44:55 +0530

    commit

--- Assignment/jukebox.py
+++ Assignment/jukebox.py
@@ -1,3 +1,13 @@
 from test_tuplenested import albums
 
-print(albums)
+while True:
+    print("Please choose your album (invalid choice exits):")
+    # for index, (title, artist, year, songs) in enumerate(albums):
+    #     print("{}: {}, {}, {}, {}".format(index + 1, title, artist, year, songs))
+
+    for index, value in enumerate(albums):
+        title, artist, year, songs = value
+        print(index, title, artist, year, songs)
+
+    break
+
