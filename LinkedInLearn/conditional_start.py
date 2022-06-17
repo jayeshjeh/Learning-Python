da4b51e7bedefc8cf95ba9b0cf6c1b1d32ad3c39
jayeshjeh <jay.parmar.11169@gmail.com>
2022-06-17 19:12:10 +0530

    commit

---
+++ LinkedInLearn/conditional_start.py
@@ -0,0 +1,27 @@
+#
+# Example file for working with conditional statements
+# LinkedIn Learning Python course by Joe Marini
+#
+
+
+def main():
+    x, y = 100, 100
+
+    # conditional flow uses if, elif, else
+    if x < y:
+        result = "x is less than y"
+    elif x == y:
+        result = " x is same as y"
+    else:
+        result = "x is greater than y"
+
+    print(result)
+
+    # conditional statements let you use "a if C else b"
+
+    # match-case makes it easy to compare multiple values
+    value = "one"
+
+
+if __name__ == "__main__":
+    main()
