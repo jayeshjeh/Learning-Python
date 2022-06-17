4a565a4d4e250ac389f90d5f37896952efb375bb
jayeshjeh <jay.parmar.11169@gmail.com>
2022-06-17 19:28:09 +0530

    commit

--- LinkedInLearn/loops_start.py
+++ LinkedInLearn/loops_start.py
@@ -22,14 +22,17 @@ def main():
     #     print(d)
 
     # TODO: use the break and continue statements
-    for x in range(5, 10):
-        # if x == 7:
-        #     break
-        if x % 2 == 0:
-            continue
-        print(x)
+    # for x in range(5, 10):
+    #     if x == 7:
+    #         break
+    #     if x % 2 == 0:
+    #         continue
+    #     print(x)
 
     # TODO: using the enumerate() function to get index
+    days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    for i, d in enumerate(days):
+        print(i, d)
 
 
 if __name__ == "__main__":
