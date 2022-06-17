5d5c6db5cd8b9dcf9189134a3a7c9416d22afaa9
jayeshjeh <jay.parmar.11169@gmail.com>
2022-06-17 22:50:36 +0530

    commit

---
+++ LinkedInLearn/Files/shell.py
@@ -0,0 +1,30 @@
+#
+# Example file for working with filesystem shell methods
+# LinkedIn Learning Python course by Joe Marini
+#
+
+import os
+from os import path
+import shutil
+
+def main():
+    # make a duplicate of an existing file
+    if path.exists("textfile.txt"):
+        src = path.realpath("textfile.txt")
+
+
+# get the path to the file in the current directory
+    dst = src + ".bak"
+    shutil.copy(src, dst)
+
+# let's make a backup copy by appending "bak" to the name
+
+# rename the original file
+
+# now put things into a ZIP archive
+
+# more fine-grained control over ZIP files
+
+
+if __name__ == "__main__":
+    main()
