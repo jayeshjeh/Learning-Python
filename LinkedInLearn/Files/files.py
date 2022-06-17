c6e2e8301734b3cac86e14298d8ae740013325fe
jayeshjeh <jay.parmar.11169@gmail.com>
2022-06-17 22:30:06 +0530

    commit

--- LinkedInLearn/Files/files.py
+++ LinkedInLearn/Files/files.py
@@ -9,16 +9,23 @@ def main():
 #     myfile = open("textfile.txt", "w+")
 
 # Open the file for appending text to the end
-    myfile = open("textfile.txt", "a+")
+#     myfile = open("textfile.txt", "a+")
 
 # write some lines of data to the file
-    for i in range(10):
-        myfile.write("This is some new text\n")
-
+#     for i in range(10):
+#         myfile.write("This is some new text\n")
+#
 # close the file when done
-    myfile.close()
+#     myfile.close()
 
 # Open the file back up and read the contents
+    myfile = open("textfile.txt", "r")
+    if myfile.mode == "r":
+        # contents = myfile.read()
+        # print(contents)
+        fl = myfile.readlines()
+        for x in fl:
+            print(x)
 
 
 if __name__ == "__main__":
