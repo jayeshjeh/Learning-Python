ebd31ee57b893ab1a60b19ce1458e7f40fdd2352
jayeshjeh <jay.parmar.11169@gmail.com>
2022-07-21 10:47:56 +0530

    commit

--- DataCamp/basic1.py
+++ DataCamp/basic1.py
@@ -3,6 +3,16 @@ import matplotlib.pyplot as plt
 year = [1950, 1970, 1990, 2010]
 pop = [2.519, 3.692, 5.263, 6.972]
 
-# plt.plot(year, pop)
-plt.scatter(year, pop)
+# Print the last item from year and pop
+
+print(year[-1])
+print(pop[-1])
+
+# Import matplotlib.pyplot as plt
+
+# Make a line plot: year on the x-axis, pop on the y-axis
+plt.plot(year, pop)
+
+
+# Display the plot with plt.show()
 plt.show()
