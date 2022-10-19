ba9f84437418baeea853cd1a610570702060b46a
jayeshjeh <jay.parmar.11169@gmail.com>
2022-10-19 19:28:25 +0530

    commit

---
+++ Lex/for.py
@@ -0,0 +1,22 @@
+
+a = 1
+b = 10
+c = 2
+
+for a in range(a, b, c):
+    print(a)
+
+for num in range(1, 5):
+    print("The current num is ", num)
+
+print("--------------------------")
+
+for num in range(1, 7, 2):
+    print(num)
+
+print("--------------------------")
+
+
+for num in range(5, 0, -1):
+    print(num)
+
