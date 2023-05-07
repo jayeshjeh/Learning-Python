25551e28221b00db600cd9fafcc089e6fa37ee2e
jayeshjeh <jay.parmar.11169@gmail.com>
2023-05-07 16:02:14 +0530

    Inputs

---
+++ 100days_With_Projects/day1_welcome.py
@@ -0,0 +1,35 @@
+# print("Day 1 - String Manipulation")
+# print('String Concatenation is done with the "+" sign.')
+# print('e.g. print("Hello " + "world"')
+# print("New lines can be created with a backslash and n.")
+      
+# print(len(input("What is your name? ")))
+
+# # 🚨 Don't change the code below 👇
+# a = input("a: ")
+# b = input("b: ")
+# # 🚨 Don't change the code above 👆
+
+# ####################################
+# #Write your code below this line 👇
+
+# c = a;
+# a = b;
+# b = c;
+
+
+# #Write your code above this line 👆
+# ####################################
+
+# # 🚨 Don't change the code below 👇
+# print("a: " + a)
+# print("b: " + b)
+
+
+print("Welcome to the band name generator")
+
+city = input("Which city did you grow up in?\n")
+
+pet = input("Name of your pet is?\n")
+
+print("Band name is: ", city + " " + pet)
--- 100days_With_Projects/welcome.py
+++
@@ -1,7 +0,0 @@
-print("Welcome to the band name generator")
-
-city = input("Which city did you grow up in?\n")
-
-pet = input("Name of your pet is?\n")
-
-print("Band name is: ", city + " " + pet)
