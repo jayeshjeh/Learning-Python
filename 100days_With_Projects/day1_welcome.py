7f5dfe39f1b921decef77075078c55316258c041
jayeshjeh <jay.parmar.11169@gmail.com>
2023-09-26 21:16:12 +0530

    commit

--- 100days_With_Projects/day1_welcome.py
+++ 100days_With_Projects/day1_welcome.py
@@ -33,3 +33,10 @@ city = input("Which city did you grow up in?\n")
 pet = input("Name of your pet is?\n")
 
 print("Band name is: ", city + " " + pet)
+
+# print("Day 1 - String Manipulation")
+# print("""String Concatenation is done with the "+" sign.""")
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+# print(len(input("What is your name? ")))
